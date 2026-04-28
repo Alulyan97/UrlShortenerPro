658730edbb75f1f38c632887b703202bd0f51536
Alulyan Petros <alulyanp@mail.ru>
2026-04-29 01:08:05 +0300

    Подключение к Redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,1 +1,21 @@
-// TODO: Реализовать подключение к Redis
+const redis = require("redis");
+
+const client = redis.createClient({
+    url: process.env.REDIS_URL
+});
+
+client.on("error", (err) => {
+    console.error("Ошибка подключения к Redis:", err);
+});
+
+client.on("connect", () => {
+    console.log("Redis подключен");
+});
+
+(
+    async () => {
+        await client.connect();
+    }
+)();
+
+module.exports = client;
--- src/controllers/redirectController.js
+++ src/controllers/redirectController.js
@@ -1,46 +1,80 @@
 const linkModel = require("../models/linkModel");
 const analyticsModel = require("../models/analyticsModel");
 const geoip = require("geoip-lite");
+const redis = require("../config/redis");
 
 const controller = {
-    async redirect (req, res) {
+    async redirect(req, res) {
         try {
             const { shortCode } = req.params;
 
-            const link = await linkModel.findByShortCode(shortCode);
-
-            if(!link) {
-                return res.status(404).json({ error: "Ссылка не найдена"});
+            const verificationUrl = await redis.get(`${shortCode}`);
+            if (verificationUrl) {
+                setImmediate(async () => {
+                    try {
+                        const link = await linkModel.findByShortCode(shortCode);
+                        if (link) {
+                            const ip = req.ip || req.connection.remoteAddress;
+                            let country = "Unknown";
+                            let city = "Unknown";
+                            if (ip === "::1" || ip === "127.0.0.1") {
+                                country = "Local";
+                                city = "Local";
+                            } else {
+                                const geo = geoip.lookup(ip);
+                                if (geo) {
+                                    country = geo.country || "Unknown";
+                                    city = geo.city || "Unknown";
+                                }
+                            }
+                            await analyticsModel.create(
+                                link.id, ip,
+                                req.get("User-Agent") || null,
+                                req.get("Referer") || null,
+                                country, city
+                            );
+                        }
+                    } catch (err) {
+                        console.error("Ошибка сохранения клика:", err);
+                    }
+                });
+                return res.redirect(302, verificationUrl);
             }
 
+            const link = await linkModel.findByShortCode(shortCode);
+            if (!link) {
+                return res.status(404).json({ error: "Ссылка не найдена" });
+            }
+
+            await redis.set(`link:${shortCode}`, link.original_url, {
+                EX: 3600
+            });
+
             setImmediate(async () => {
-                try{
+                try {
                     const ip = req.ip || req.connection.remoteAddress;
                     let country = "Unknown";
                     let city = "Unknown";
-
                     if (ip === "::1" || ip === "127.0.0.1") {
                         country = "Local";
-                        city = "local";
+                        city = "Local";
                     } else {
                         const geo = geoip.lookup(ip);
-                        if(geo){
+                        if (geo) {
                             country = geo.country || "Unknown";
                             city = geo.city || "Unknown";
                         }
                     }
                     await analyticsModel.create(
-                        link.id,
-                        ip,
+                        link.id, ip,
                         req.get("User-Agent") || null,
                         req.get("Referer") || null,
-                        country,
-                        city
+                        country, city
                     );
-            } catch (err) {
+                } catch (err) {
                     console.error("Ошибка сохранения клика:", err);
-                        }
-            })
+                }
+            });
 
             return res.redirect(302, link.original_url);
         } catch (err) {
@@ -48,6 +82,6 @@ const controller = {
             res.status(500).json({ error: "Ошибка сервера" });
         }
     }
-}
+};
 
 module.exports = controller;
